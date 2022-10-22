f97059478b2f10ecd38d734cdf7f4d8b0a88226d
Cabba4 <anmol.arora@tuni.fi>
2022-10-23 02:27:31 +0300

    Separate independent contacts service added to the project

--- src/main/java/com/ar21/anmolarora/App.java
+++ src/main/java/com/ar21/anmolarora/App.java
@@ -9,6 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        //IDMfunction();
+        ContactService();
+    }
+
+    public static void IDMfunction(){
         IDMService idm = new IDMService();
         idm.createIdentity(new Student("Ethan","Blake","ethan.blake@hackermail.com"));
         Student newStudent = new Student();
@@ -17,5 +22,12 @@ public class App
         idm.getIdentity(newTeacher);
         newTeacher.addDetails();
         newTeacher.displayName();
+    }
+
+    public static void ContactService(){
+        ContactService contactService = new ContactService();
+        CustomerContact another = new CustomerContact("user1", "user@mail.com", 1121);
+        contactService.addContact(another);
+        contactService.findContact(another);
     }
 }
